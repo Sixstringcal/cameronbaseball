a24c21ddc0e435bb8d93d02d3dab72071c1241ee
sixstringcal <sixstringcal@gmail.com>
2020-12-26 21:48:27 -0700

    Video updates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+
 import Home from "./Home";
-import ReactDOM from 'react-dom';
 
 import Videos from "./Videos";
 import { makeStyles } from '@material-ui/core/styles';
 import NavBar from "./NavBar";
-import { Route, Switch, BrowserRouter, Link } from "react-router-dom";
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { Backdrop, colors } from '@material-ui/core';
-import { blue } from '@material-ui/core/colors';
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 
 const useStyles = makeStyles((theme) => ({
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,23 @@
 import './App.css';
 import AppBar from '@material-ui/core/AppBar';
+import logo from './cameronlogo.png';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <AppBar position="static">
+        <AppBar position="static"  >
             <Toolbar>
                 <IconButton edge="start" color="inherit" aria-label="menu">
                     {/*<MenuIcon />*/}
+                        <Link to="/">
+                        <img src={logo} alt="logo" className="icon" />
+                        </Link>
                 </IconButton>
+                <logo />
                 <Typography>
                     <Link to="/">
                         Home
